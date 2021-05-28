78c37c462d4d1e8eab7cc1be4fee9912316e5fba
raquelbennington <raquelbennington@gmail.com>
2021-05-28 08:39:14 -0300

    tchaki tchaki button

--- components/Card.vue
+++ components/Card.vue
@@ -16,26 +16,15 @@
         </div>
         <div class="flex items-center">
           <div class="text-gray-600 font-bold">{{ price }}</div>
-          <button
-            href="javascript:;"
-            class="rounded-full bg-black text-white hover:bg-white hover:text-black hover:shadow-xl focus:outline-none w-10 h-10 flex ml-auto transition duration-200"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              class="stroke-current m-auto"
-            >
-              <line x1="12" y1="5" x2="12" y2="19"></line>
-              <line x1="5" y1="12" x2="19" y2="12"></line>
-            </svg>
-          </button>
+
+           <div class="hidden md:flex items-center justify-end md:flex-1 lg:w-0 py-2">
+    <a
+      href="#"
+      class="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-bold text-black hover:text-gray-500 border-black border-2 border-t border-b border-r"
+      >comprar
+    </a>
+  </div>
+
         </div>
       </div>
     </div>
--- components/x-rascunho.vue
+++ components/x-rascunho.vue
@@ -24,3 +24,25 @@
 //     lazy: true,
 //     langDir: '~/lang/'
 //   },
+
+
+<button
+            href="javascript:;"
+            class="rounded-full bg-black text-white hover:bg-white hover:text-black hover:shadow-xl focus:outline-none w-10 h-10 flex ml-auto transition duration-200"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              class="stroke-current m-auto"
+            >
+              <line x1="12" y1="5" x2="12" y2="19"></line>
+              <line x1="5" y1="12" x2="19" y2="12"></line>
+            </svg>
+          </button>
